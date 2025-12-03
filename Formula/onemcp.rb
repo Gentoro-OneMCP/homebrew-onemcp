3048fdc2148201b19b0fcd9cb0c783f59709785d
Shubham Singla <shubham@gentoro.com>
2025-12-03 09:48:07 -0800

    Update manifest for cli-v0.0.3

--- Formula/onemcp.rb
+++ Formula/onemcp.rb
@@ -2,21 +2,21 @@ class Onemcp < Formula
   desc "CLI for Gentoro OneMCP - Connect APIs to AI models via Model Context Protocol"
   homepage "https://github.com/Gentoro-OneMCP/onemcp"
   license "Apache-2.0"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.2/onemcp-darwin-arm64.tar.gz"
-      sha256 "10bd78a952aa17db9a37bdf40483d5138579f8936b18cd37699f4fe5ed9831f4"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-darwin-arm64.tar.gz"
+      sha256 "bd577241e37c72e51f32fde7788e2e39b20f5998e92fd1f733a418f9f504a140"
     else
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.2/onemcp-darwin-amd64.tar.gz"
-      sha256 "9a35215cc99bc7ee405ecc6e18894fd55018cbbfb043ebf09e9c998d1b9a22c2"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-darwin-amd64.tar.gz"
+      sha256 "6d10813bd23e7a3831a05f880670da683a577bd5b7a202b2713c0d27e041fe9d"
     end
   end
 
   on_linux do
-    url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.2/onemcp-linux-amd64.tar.gz"
-    sha256 "eb7c4538717bdbeee174c390ff918f30b31994caa6aa30f5226f9c9a8c796a4d"
+    url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-linux-amd64.tar.gz"
+    sha256 "e0659a98872c83184389c319de957c2252474612d994b1f0cddd867a7a5ba07a"
   end
 
   depends_on "docker" => :recommended
