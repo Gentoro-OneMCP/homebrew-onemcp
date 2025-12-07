aa2ad151d0f966b20b013713f1c700ddd8a6db85
Jun Yang <jun@gentoro.com>
2025-12-07 14:23:15 -0500

    Update formula to cli-v0.0.4

--- Formula/onemcp.rb
+++ Formula/onemcp.rb
@@ -2,21 +2,36 @@ class Onemcp < Formula
   desc "CLI for Gentoro OneMCP - Connect APIs to AI models via Model Context Protocol"
   homepage "https://github.com/Gentoro-OneMCP/onemcp"
   license "Apache-2.0"
-  version "0.0.3"
+  version "0.0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-darwin-arm64.tar.gz"
-      sha256 "bd577241e37c72e51f32fde7788e2e39b20f5998e92fd1f733a418f9f504a140"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-darwin-arm64.tar.gz"
+      sha256 "52f0d0879d09d1d7de7285d3b6f829316c2dc6736484a2ccb796bd14b7815921"
     else
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-darwin-amd64.tar.gz"
-      sha256 "6d10813bd23e7a3831a05f880670da683a577bd5b7a202b2713c0d27e041fe9d"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-darwin-amd64.tar.gz"
+      sha256 "48777eef34f35e9b9a11aad4e2710c0fbdf3d87fd0a0cce3ba9cb56b7e683db8"
     end
   end
 
   on_linux do
-    url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.3/onemcp-linux-amd64.tar.gz"
-    sha256 "e0659a98872c83184389c319de957c2252474612d994b1f0cddd867a7a5ba07a"
+    if Hardware::CPU.is_64_bit?
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-linux-amd64.tar.gz"
+      sha256 "2368e90ac2b50130a63aa16b22ff7f402267b0d41fd5770c974670f60ae16a01"
+    else
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-linux-386.tar.gz"
+      sha256 "d9d3f3cf9fb89d483020e2f5eea5c8e850e4b67ba35742262e315706798232a5"
+    end
+  end
+
+  on_windows do
+    if Hardware::CPU.is_64_bit?
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-windows-amd64.zip"
+      sha256 "b0c18ebe41658ad7a08f3b8bbba8e18872ecf19d306f1b849b50779c993eca80"
+    else
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-windows-386.zip"
+      sha256 "9e3aabe58d1bc046228cb48fd03f310f5d44507c401ce02881c92bb5ce32a7b1"
+    end
   end
 
   depends_on "docker" => :recommended
