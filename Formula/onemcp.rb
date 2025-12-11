58a55b4ec2b77f997d6eea333a84ee01f1c20085
Shubham Singla <shubham@gentoro.com>
2025-12-10 18:49:01 -0800

    Update formula to cli-v0.0.5

--- Formula/onemcp.rb
+++ Formula/onemcp.rb
@@ -2,25 +2,25 @@ class Onemcp < Formula
   desc "CLI for Gentoro OneMCP - Connect APIs to AI models via Model Context Protocol"
   homepage "https://github.com/Gentoro-OneMCP/onemcp"
   license "Apache-2.0"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-darwin-arm64.tar.gz"
-      sha256 "67cc447e5a5c55b35d1fafa972b71f28fbddf2d3b09451d534e7f617dce13aa1"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.5/onemcp-darwin-arm64.tar.gz"
+      sha256 "d87bc312ad345d9493b959e5a1a41216150e16c6bb8065b1d05b60fae20e712a"
     else
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-darwin-amd64.tar.gz"
-      sha256 "e7c87fc9c1855c94d68297df87a744c776e3bd41189e666052cbb2aa1dac7bc1"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.5/onemcp-darwin-amd64.tar.gz"
+      sha256 "f80bc795775fcfd948c9f1655645db71d3626e7968ef8e065bfc9f43bfd335a3"
     end
   end
 
   on_linux do
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-linux-amd64.tar.gz"
-      sha256 "6e2ce6a6b1bea2954dd473824c7b14879a45ff21c4751e6e15bf9fec3eaba0a5"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.5/onemcp-linux-amd64.tar.gz"
+      sha256 "b5c5da8eae96f9c3a79c4955d82ee5ac3e0c123b860f5f50c2d2d5b36a7d80b8"
     else
-      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.4/onemcp-linux-386.tar.gz"
-      sha256 "77920a6e8092740c0509b19114743e33c93b20344d4a5e1ec7ebab2bca331511"
+      url "https://github.com/Gentoro-OneMCP/onemcp/releases/download/cli-v0.0.5/onemcp-linux-386.tar.gz"
+      sha256 "8d62bd0221f09278f708966b223176ca79ce4bca3125c07eaae3cb41c363a167"
     end
   end
 
